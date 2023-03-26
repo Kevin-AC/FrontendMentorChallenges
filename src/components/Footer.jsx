529f2118a85b89bbe3111833bfb39497fce00891
kevin.argumedo <kevin.argumedo@gmail.com>
2023-03-26 15:38:15 -0500

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer () {
+  return (
+    <div className='text-center font-medium text-sm text-Verydarkblue my-2'>
+      Challenge by{' '}
+      <a
+        className='text-Blue'
+        href='https://www.frontendmentor.io?ref=challenge'
+        target='_blank' rel='noreferrer'
+      >
+        Frontend Mentor.
+      </a>{' '}
+      <br />
+      Coded by{' '}
+      <a className='text-Blue' href='https://www.frontendmentor.io/profile/Kevin-AC' target='_blank' rel='noreferrer'>
+        Kevin Argumedo
+      </a>
+      .
+    </div>
+  )
+}
+export default Footer
