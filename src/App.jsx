9810c4a15ac7d5661658f01075e24987d314fbb8
kevin.argumedo <kevin.argumedo@gmail.com>
2023-03-29 19:16:23 -0500

    add  footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Button from './components/Button'
 import Card from './components/Card'
-
+import Footer from './components/Footer'
 function App () {
   const [data, setData] = useState([])
   useEffect(() => {
@@ -15,30 +15,32 @@ function App () {
   const result = Math.round(data.reduce((a, c) => a + c.score, 0) / 4)
 
   return (
-    <main className='h-screen xl:h-auto xl:flex bg-white xl:rounded-3xl xl:shadow-2xl'>
-      <section className='w-full h-auto  xl:w-[370px] xl:h-[500px] flex flex-col items-center gap-5 xl:gap-7 py-7 px-14 xl:px-22 rounded-b-3xl xl:rounded-3xl bg-gradient-to-b from-Lightslateblue_background to-Lightroyalblue_background'>
-        <h1 className='text-xl xl:text-2xl text-Lightlavender'>Your Result</h1>
-        <div className='w-32 h-32 xl:w-48 xl:h-48 rounded-full flex flex-col items-center justify-center  bg-gradient-to-b from-Violetblue_circle to-Persianblue_circle'>
-          <p className='text-white text-5xl xl:text-7xl font-extrabold'>{result}</p>
-          <span className='text-base text-Lightlavender'>of 100</span>
-        </div>
-        <p className='text-2xl xl:text-4xl font-bold text-White'>Great</p>
-        <p className='text-center text-Lightlavender text-lg '>You scored higher than 65% of the people who have taken these tests.</p>
-      </section>
-      <section className='p-4 xl:px-11 flex flex-col gap-4 xl:gap-6 xl:pt-7 xl:w-[370px]  '>
-        <h2 className='text-Darkgrayblue text-lg xl:text-2xl font-bold '>Summary</h2>
-        {data.map((item, index) => (
-          <Card
-            key={index}
-            imgSrc={item.icon}
-            tipe={item.category}
-            score={item.score}
-          />
-        ))}
-        <Button />
-      </section>
-
-    </main>
+    <div className='w-full h-auto grid place-content-center xl:min-h-screen gap-3'>
+      <main className=' xl:flex bg-white xl:rounded-3xl xl:shadow-2xl '>
+        <section className='w-full h-auto  xl:w-[370px] xl:h-[500px] flex flex-col items-center gap-5 xl:gap-7 py-10 px-14 xl:px-22 rounded-b-3xl xl:rounded-3xl bg-gradient-to-b from-Lightslateblue_background to-Lightroyalblue_background'>
+          <h1 className='text-xl xl:text-2xl text-Lightlavender'>Your Result</h1>
+          <div className='w-32 h-32 xl:w-48 xl:h-48 rounded-full flex flex-col items-center justify-center  bg-gradient-to-b from-Violetblue_circle to-Persianblue_circle'>
+            <p className='text-white text-5xl xl:text-7xl font-extrabold'>{result}</p>
+            <span className='text-base text-Lightlavender'>of 100</span>
+          </div>
+          <p className='text-2xl xl:text-4xl font-bold text-White'>Great</p>
+          <p className='text-center text-Lightlavender text-lg '>You scored higher than 65% of the people who have taken these tests.</p>
+        </section>
+        <section className='p-4 xl:px-11 flex flex-col gap-4 xl:gap-6 xl:pt-7 xl:w-[370px]  '>
+          <h2 className='text-Darkgrayblue text-lg xl:text-2xl font-bold '>Summary</h2>
+          {data.map((item, index) => (
+            <Card
+              key={index}
+              imgSrc={item.icon}
+              tipe={item.category}
+              score={item.score}
+            />
+          ))}
+          <Button />
+        </section>
+      </main>
+      <Footer />
+    </div>
 
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer () {
+  return (
+    <footer className='text-center  font-bold text-Darkgrayblue'>
+      Challenge by {' '}
+      <a className='text-Cobaltblue' href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>
+        Frontend Mentor.
+      </a> <br />
+      Coded by {' '}
+      <a className='text-Cobaltblue' href='https://www.frontendmentor.io/profile/Kevin-AC' target='_blank' rel='noreferrer'>
+        Kevin Argumedo
+      </a>
+      .
+    </footer>
+  )
+}
+export default Footer
