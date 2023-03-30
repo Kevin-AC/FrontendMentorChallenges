ade9a53581e606505264121007ac53ffb35bae05
kevin.argumedo <kevin.argumedo@gmail.com>
2023-03-29 19:43:43 -0500

    hover button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button () {
   return (
-    <button className='w-full h-14 rounded-full bg-Darkgrayblue text-White font-semibold text-xl'>
+    <button className='w-full h-14 rounded-full bg-Darkgrayblue text-White font-semibold text-xl hover:bg-gradient-to-b from-Lightslateblue_background to-Lightroyalblue_background'>
       Continue
     </button>
   )
