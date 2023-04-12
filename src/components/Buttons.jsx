d6893749b883db1548be4d61b043e150d37b163b
kevin.argumedo <kevin.argumedo@gmail.com>
2023-04-12 18:17:28 -0500

    Button on nav menu mobile

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,6 +1,10 @@
 function Buttons ({ text }) {
   return (
-    <button className={`boton ${text === 'Learn More' ? 'bg-none text-White-text' : 'bg-White-text'}`}>
+    <button className={`
+      boton ${text === 'Learn More'
+        ? 'bg-none text-White-text'
+        : text === 'Sign Up' ? 'text-White-text bg-gradient-to-r from-Verylightred to-Lightred' : 'bg-White-text'}`}
+    >
       {text}
     </button>
   )
