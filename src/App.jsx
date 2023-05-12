bc636446e4596a0884623ba62701173218899aa9
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-12 18:59:24 -0500

    mobile retoque

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App () {
       <Header />
       <section className="w-auto h-auto my-10 flex flex-col sm:flex-row gap-10 px-[7%] sm:items-center overflow-hidden">
         <img className='lg:order-1 -mr-72' width={800} src="/images/illustration-devices.svg" alt="mainIlustration" />
-        <article className='w-[500px] flex flex-col gap-4 lg:gap-5'>
+        <article className='w-auto lg:w-[500px] flex flex-col gap-4 lg:gap-5'>
           <div className="flex items-center gap-4 text-xl uppercase font-BarlowCondensed tracking-widest">
             <p className="bg-VeryDarkBlue text-white text-center rounded-full font-semibold px-3 py-1">New</p>
             <p className="font-normal text-DarkGrayishBlue ">Monograph Dashboard</p>
