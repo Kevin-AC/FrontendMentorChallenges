68a5a3b8f813603549818844da19d4d5a300aa58
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-12 18:51:04 -0500

    proyecto listo

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line react/prop-types
 export function Button ({ text, style }) {
   return (
-    <button className={`w-40 h-10 bg-Red uppercase font-bold font-BarlowCondensed ${style}`}>{text}</button>
+    <button className={`w-40 h-10 bg-Red  uppercase font-bold font-BarlowCondensed outline-none ${style}`}>{text}</button>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,12 +8,12 @@ export function Header () {
       <img src="/images/logo.svg" alt="Logo Web" />
       <div onClick={() => setOpen(!open)} className={`fas text-2xl text-VeryDarkBlue sm:hidden  ${open ? 'fa-bars' : 'fa-times'}`}></div>
       <nav className={`nav ${!open ? 'flex' : 'hidden sm:flex'}`}>
-        <ul className="text-VeryDarkBlue font-BarlowCondensed font-bold uppercase text-center flex flex-col sm:flex-row gap-4">
+        <ul className="ulNav text-VeryDarkBlue font-BarlowCondensed font-bold uppercase text-center flex flex-col sm:flex-row gap-4">
           <li><a href="#">product</a></li>
           <li><a href="#">features</a></li>
           <li><a href="#">pricing</a></li>
         </ul>
-        <Button text='Login' style='bg-transparent text-GrayishBlue w-20' />
+        <Button text='Login' style='bg-transparent text-GrayishBlue w-20 hover:underline' />
       </nav>
 
     </section>
