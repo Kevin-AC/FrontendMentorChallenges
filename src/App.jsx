d19721a2ca82a109bc679f31b5027569197a023b
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-12 16:53:34 -0500

    tablet view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { Header } from './Components/Header'
-import { MainSection } from './MainSection'
+import { Button } from './Components/Button'
 
 function App () {
   return (
-    <main className="w-screen h-auto text-lg font-BarlowCondensed">
+    <main className="w-full h-auto text-lg font-BarlowCondensed">
       <Header />
-      <MainSection />
+      <section className="my-10 flex flex-col gap-10 p-4 sm:items-center">
+        <img src="/images/illustration-devices.svg" alt="mainIlustration" />
+        <div className="flex items-center gap-4 text-xl uppercase font-BarlowCondensed tracking-widest">
+          <p className="bg-VeryDarkBlue text-white rounded-full font-semibold px-3 py-1">New</p>
+          <p className="font-normal text-DarkGrayishBlue ">Monograph Dashboard</p>
+        </div>
+        <div className="flex flex-col gap-4 ">
+          <h1 className="text-VeryDarkBlue uppercase text-5xl font-semibold font-BarlowCondensed">Powerful insights into your team</h1>
+          <p className="text-xl text-DarkGrayishBlue pr-14 font-Barlow">Project planning and time tracking for agile teams</p>
+        </div>
+        <div className="flex gap-4">
+          <Button text='schedule a demo' style='text-white rounded-lg' />
+          <Button text='to see a preview' style='bg-transparent tracking-widest text-DarkGrayishBlue font-normal' />
+        </div>
+    </section>
     </main>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,14 +6,14 @@ export function Header () {
   return (
     <section className=" w-full h-auto px-[7%] mt-10 flex justify-between items-center relative">
       <img src="/images/logo.svg" alt="Logo Web" />
-      <div onClick={() => setOpen(!open)} className={`fas text-2xl text-VeryDarkBlue  ${open ? 'fa-bars' : 'fa-times'}`}></div>
-      <nav className={`nav ${!open ? 'flex' : 'hidden'}`}>
-        <ul className="text-VeryDarkBlue font-BarlowCondensed font-bold uppercase text-center flex flex-col gap-4 ">
+      <div onClick={() => setOpen(!open)} className={`fas text-2xl text-VeryDarkBlue sm:hidden  ${open ? 'fa-bars' : 'fa-times'}`}></div>
+      <nav className={`nav ${!open ? 'flex' : 'hidden sm:flex'}`}>
+        <ul className="text-VeryDarkBlue font-BarlowCondensed font-bold uppercase text-center flex flex-col sm:flex-row gap-4">
           <li><a href="#">product</a></li>
           <li><a href="#">features</a></li>
           <li><a href="#">pricing</a></li>
         </ul>
-        <Button text='Login' style='bg-transparent text-GrayishBlue' />
+        <Button text='Login' style='bg-transparent text-GrayishBlue w-20' />
       </nav>
 
     </section>
