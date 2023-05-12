db1d4f359ccc176fba6a0b673ecf0b67f5e12ba5
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-12 16:04:25 -0500

    moblie view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from './Components/Header'
+import { MainSection } from './MainSection'
 
 function App () {
   return (
     <main className="w-screen h-auto text-lg font-BarlowCondensed">
       <Header />
+      <MainSection />
     </main>
   )
 }
