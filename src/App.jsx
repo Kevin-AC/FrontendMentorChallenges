fb0c6216ec7710a980310c8663c44be8083be514
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-13 17:07:09 -0500

    margin main section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App () {
     <main className="w-full h-auto text-lg font-BarlowCondensed relative z-50 overflow-hidden">
       <div className='z-0 absolute -right-4 lg:-right-32 -top-20 w-3/5 h-96 bg-LightGrayishBlue rounded-b-[40px]'></div>
       <Header />
-      <section className="w-auto h-auto my-10 flex flex-col lg:flex-row gap-10 px-[7%] sm:items-center overflow-hidden">
+      <section className="w-auto h-auto lg:mt-20 mt-10 flex flex-col lg:flex-row gap-10 px-[7%] sm:items-center overflow-hidden">
         <img className='lg:order-1 lg:-mr-72 z-40' width={800} src="/images/illustration-devices.svg" alt="mainIlustration" />
         <article className='w-full lg:w-[500px] flex flex-col gap-4 lg:gap-5 z-50'>
           <div className="flex items-center gap-4 text-xl uppercase font-BarlowCondensed tracking-widest">
