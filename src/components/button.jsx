b5af9f3ec734696e2fb536849d3d21946c1906ab
Kevin-AC <kevin.argumedo@gmail.com>
2025-11-03 22:33:09 -0500

    chore:alineado de items en cards

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,6 +1,6 @@
 function Button({title, className}) {
     return(
-        <button className={` m-0 p-0 grid place-content-center bg-Neutral-800 text-white px-4 py-2 rounded-3xl border border-Neutral-600 transition ${className}`}>
+        <button className={` m-0 p-0 grid place-content-center bg-Neutral-800 hover:bg-Red-500 hover:text-Neutral-900 text-white px-4 py-2 rounded-3xl border border-Neutral-600 transition ${className}`}>
             {title} 
         </button>
     );
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,8 +2,8 @@ import Button from "./button";
 
 function Card({title, description,img,isActive}){
     return(
-        <div className="w-full border border-Neutral-300 rounded-2xl bg-Neutral-800 p-4 text-Neutral-0">
-            <section className="flex gap-4">
+        <div className="w-full border border-Neutral-300 rounded-2xl bg-Neutral-800 p-3 text-Neutral-0">
+            <section className="flex items-start gap-3">
                 <img src={img} alt="logo de servicios" />
                 <div>
                     <h2 className="font-bold">{title}</h2>
@@ -12,7 +12,7 @@ function Card({title, description,img,isActive}){
             </section>
             <section className="flex justify-between items-center pt-4">
                 <Button title={"Remove"} className={'h-8 w-20'}/>
-                <input type="checkbox" checked={isActive} className="toggle toggle-sm border-0 checked:bg-Red-400" />
+                <input type="checkbox" defaultChecked={isActive} className="toggle toggle-sm border-0 checked:bg-Red-400" />
             </section>
             
 
