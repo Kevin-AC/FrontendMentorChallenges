885be49ca68333097afd1c7ac553b1f200ba1dd3
Kevin-AC <kevin.argumedo@gmail.com>
2025-12-28 19:46:51 -0500

    feat: agregar título  principal

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
     return (
         <main className="bg-Neutral-900 w-full h-screen p-4">
             <Header/>
+            <h1 className="text-Neutral-0 font-bold text-5xl tracking-widest text-center py-12 ">How’s the sky looking today?</h1>
     
             
 
