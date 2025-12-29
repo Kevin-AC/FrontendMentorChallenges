84fdf6489375929bc921cbba3c2a57322da06a5d
Kevin-AC <kevin.argumedo@gmail.com>
2025-12-28 21:39:24 -0500

    feat:(Component) seccion busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Button from "./components/Button"
+import Search from "./components/Search"
 export default function App() {
     return (
         <main className="bg-Neutral-900 w-full h-screen p-4">
             <Header/>
             <h1 className="text-Neutral-0 font-bold text-5xl tracking-widest text-center py-12 ">How’s the sky looking today?</h1>
-    
+            <Search/>
+            
             
 
         </main>
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+export default function Search(){
+    return(
+        <div className="w-full h-auto font-[DMSans] text-[20px] font-medium tracking-wider">
+            <button className="flex items-center gap-3 py-4 px-5 w-full h-14 bg-Neutral-800 opacity-100 rounded-[12px]  text-Neutral-200">
+                <img className="w-5" src="public/assets/images/icon-search.svg" alt="icon-search" />
+                    Search for a place...
+            </button>
+            <button className="w-full h-14 bg-Blue-500 rounded-[12px] text-Neutral-0 mt-3 ">
+                Search
+            </button>
+            
+        </div>
+    )
+}
