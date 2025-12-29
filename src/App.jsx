2219d9a52c7852b864beae65a3e7240b9158e6c2
Kevin-AC <kevin.argumedo@gmail.com>
2025-12-28 19:27:49 -0500

    feat(components): creación componente Nav con logo y botón de unidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Nav from "./components/Nav"
 export default function App() {
     return (
-        <main className="bg-Neutral-900 w-auto h-full">
-            
+        <main className="bg-Neutral-900 w-full h-screen p-4">
+            <Nav/>
             
 
         </main>
