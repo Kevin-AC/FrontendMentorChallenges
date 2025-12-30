4e51285e779517e7019ff6e3174986cc4b8053a1
Kevin-AC <kevin.argumedo@gmail.com>
2025-12-29 20:14:45 -0500

    fix(components): ajustar tamaño y separación de cards

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -4,7 +4,7 @@ export default function DailyForecast(){
     return(
         <section className="mt-5">
             <h2 className="font-semibold text-2xl text-Neutral-0 mb-5">Daily forecast</h2>
-            <div className="flex flex-wrap gap-4 justify-between">
+            <div className="grid grid-cols-3 gap-4">
                 <ForecastCard day={"Tue"} icon={"public/assets/images/icon-partly-cloudy.webp"} maxTemp={68} minTemp={57} />
                 <ForecastCard day={"Tue"} icon={"public/assets/images/icon-partly-cloudy.webp"} maxTemp={68} minTemp={57} />
                 <ForecastCard day={"Tue"} icon={"public/assets/images/icon-partly-cloudy.webp"} maxTemp={68} minTemp={57} />
