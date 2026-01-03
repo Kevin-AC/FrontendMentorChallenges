b3aef209a4936bfde54974aab9ba7366cc77d550
Kevin-AC <kevin.argumedo@gmail.com>
2026-01-02 21:54:35 -0500

    feat: actualizar componente HourlyForecast para usar datos dinámicos y mejorar el botón

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -2,14 +2,22 @@ import Button from "./Button";
 import DaysDropdown from "./DaysDropdown";
 import HourlyCard from "./HourlyCard";
 import { useState } from "react";
+import { useMockWeather } from "../hooks/useMockWeather";
 export default function HourlyForecast(){
-    const [isSwitchPanelOpen, setIsSwitchPanelOpen]=useState(false)
+    const [isSwitchPanelOpen, setIsSwitchPanelOpen]=useState(true)
+    const weather = useMockWeather()
+    if (!weather) return
+    const { hourly,actual }=weather
     return(
         <section className="mt-8 bg-Neutral-800 opacity-100 rounded-2xl py-5 px-4 text-Neutral-0">
             <div className="flex items-center justify-between">
                 <h2 className="font-semibold text-xl">Hourly forecast</h2>
                 <Button 
-                    text={"Tuesday"} 
+                    text={
+                        new Date(actual.time).toLocaleDateString("en-US", {
+                            weekday:"long",
+                        })
+                    }
                     Style={"bg-Neutral-600"}
                     onClick={()=>setIsSwitchPanelOpen(!isSwitchPanelOpen)}
                 
@@ -19,12 +27,14 @@ export default function HourlyForecast(){
                 isOpen={isSwitchPanelOpen}
             />
             <div className="flex flex-col gap-4 mt-6">
-                <HourlyCard hour={"3 PM"} temp={68} icon={"public/assets/images/icon-rain.webp"} />
-                <HourlyCard hour={"3 PM"} temp={68} icon={"public/assets/images/icon-rain.webp"} />
-                <HourlyCard hour={"3 PM"} temp={68} icon={"public/assets/images/icon-rain.webp"} />
-                <HourlyCard hour={"3 PM"} temp={68} icon={"public/assets/images/icon-rain.webp"} />
-                <HourlyCard hour={"3 PM"} temp={68} icon={"public/assets/images/icon-rain.webp"} />
-                <HourlyCard hour={"3 PM"} temp={68} icon={"public/assets/images/icon-rain.webp"} />
+                {hourly.time.map((hora,i)=>(
+                    <HourlyCard 
+                        key={hora}
+                        icon={"public/assets/images/icon-rain.webp"}
+                        temp={`${hourly.temperature_2m[i]}°`}
+                        hour={hourly.time[i]}
+                    />
+                ))}
             </div>
         </section>
     )
