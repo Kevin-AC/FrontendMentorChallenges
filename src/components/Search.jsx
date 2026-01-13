e1e2087e38f90273fc747a515ce562171bf6fb3b
Kevin-AC <kevin.argumedo@gmail.com>
2026-01-12 21:46:25 -0500

    fix: ajustar estilos en el componente Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,14 +10,17 @@ export default function Search(){
         setCiudadInput(value)
     }
     return(
-        <div className="w-full lg:w-2xl h-auto text-[20px] font-medium tracking-wider flex flex-col md:flex-row md:items-center md: gap-4 lg:mx-auto ">
+        <div className="w-full lg:w-2xl h-auto text-[20px] font-medium tracking-wider flex flex-col md:flex-row md:items-center md: gap-4 lg:mx-auto relative ">
+            <img src="/assets/images/icon-search.svg" alt="iconSearch" className="top-4.5  left-3 absolute ml-1.5 " />
             <input
                 type="text"
-                className="flex items-center gap-3 py-4 px-5 w-full  h-14 bg-Neutral-800 opacity-100 rounded-xl text-Neutral-200"
+                className="py-4 px-12.5 w-full  h-14 bg-Neutral-800 opacity-100 rounded-xl text-Neutral-200"
                 placeholder="Search for a place..."
                 value={buscarCiudad}
                 onChange={(e)=>setBuscarCiudad(e.target.value)}
+                
             />
+            
             <button className="w-full md:w-28 h-14 bg-Blue-500 rounded-xl text-Neutral-0  hover:cursor-pointer "
                     onClick={handleSubmit}>
                 Search
